1fc03eba71830d59393fbeb8957bf25e760593c9
AlefDosSantos <onnzytecnologia@gmail.com>
2022-09-28 19:02:15 -0300

    commit atualização 03

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var sobreRouter = require('./routes/sobre');
-
+var trabalheConoscoRouter = require('./routes/trabalheConosco')
 var app = express();
 
 // view engine setup
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/sobre', sobreRouter);
+app.use('/trabalheConosco', trabalheConoscoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
